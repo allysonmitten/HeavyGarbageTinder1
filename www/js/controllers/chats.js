d10cf98ce096cc1adc4c77da4ca44f8841e75245
Allyson <allysonmichellewhite@gmail.com>
2015-11-28 15:47:03 -0400

    First Commit
    Prior to Auth

---
+++ www/js/controllers/chats.js
@@ -0,0 +1,24 @@
+'use strict';
+
+app.controller('ChatsCtrl', function($scope, Chats, Products) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+  $scope.image ='http://www.capebretonpost.com/media/photos/unis/2012/04/10/2012-04-10-04-55-02-garbage.jpg';
+
+
+     $scope.button = function(product){
+      console.log('Button was clicked', product, $scope.image);
+      Products.saveProduct(product, $scope.image);
+  }
+
+});
